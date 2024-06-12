dcabedfade7f63605289b5d532b57c35112b7f5c
coffedahl <coffedahl@hotmail.com>
2024-06-12 21:56:46 +0200

    Added styling to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import '../app.css';
 </script>
 
-<header class="flex bg-[#1975bc] p-6">
-	<h1 class="text-3xl font-bold text-white">Worktools</h1>
+<header class="flex bg-[#1975bc] items-center h-[10vh]">
+	<h1 class="ml-8 text-3xl font-bold text-white">Worktools</h1>
 </header>
 <main>
 	<slot />
