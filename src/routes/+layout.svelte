39c446f11c3c6fec5493b6c8d5171c058d77f6dd
coffedahl <coffedahl@hotmail.com>
2024-06-12 21:27:59 +0200

    Added tailwindcss and created simple header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<header class="flex bg-[#1975bc] p-6">
+	<h1 class="text-3xl font-bold text-white">Worktools</h1>
+</header>
+<main>
+	<slot />
+</main>
